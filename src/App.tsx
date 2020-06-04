ac69b2fe11c598f068e8c8661af7806ba6488639
Nick Chaloult <npchaloult@gmail.com>
2020-06-04 15:38:39 -0400

    Add functionality to prevent "blank" components from rendering

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ import CustomButton from './components/reusable/CustomButton';
 import URLBox from './components/URLBox';
 
 const App: React.FC = () => {
-  const [repoName, setRepoName] = useState('react-ui');
+  const [repoName, setRepoName] = useState('');
   const [url, setURL] = useState('');
-  const [markdownDisplayContent, setMarkdownDisplayContent] = useState(['public', 'src']);
+  const [markdownDisplayContent, setMarkdownDisplayContent] = useState<string[]>([]);
 
   const handleURLChange = (e: React.ChangeEvent<HTMLInputElement>) => setURL(e.target.value);
 
@@ -38,8 +38,12 @@ const App: React.FC = () => {
   return (
     <div className="container container-small">
       <URLBox value={url} onChange={handleURLChange} onClick={handleGoButtonPress} />
-      <BadgesSection repoName={repoName} />
-      <MarkdownDisplay content={markdownDisplayContent} />
+      {repoName !== '' &&
+        <BadgesSection repoName={repoName} />
+      }
+      {markdownDisplayContent.length !== 0 &&
+        <MarkdownDisplay content={markdownDisplayContent} />
+      }
     </div>
   );
 };
