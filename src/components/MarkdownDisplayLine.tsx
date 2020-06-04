2758c42a1cca3d7f8d511ec81c3e973bb102e284
Nick Chaloult <npchaloult@gmail.com>
2020-06-04 16:28:14 -0400

    Darken background color of "light" background MarkdownDisplayLines

--- src/components/MarkdownDisplayLine.tsx
+++ src/components/MarkdownDisplayLine.tsx
@@ -5,7 +5,8 @@ import styled from 'styled-components';
 
 const LightBGColor = styled.div`
   padding: 0.25rem;
-  background: #414959;
+  background: #2b303b;
+  background: rgba(85, 73, 89, 0.2);
   font-family: 'Source Code Pro', monospace;
 `;
 
